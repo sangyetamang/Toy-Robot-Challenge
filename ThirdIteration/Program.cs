9df80eaa0731a5cab74391bab9750ef339c14ef2
sangyetamang <sanngye.tmg@gmail.com>
2022-08-08 18:39:53 +1000

    refactoring code

---
+++ ThirdIteration/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Iteration");
+            Simulator mySimulator = new Simulator();
+            mySimulator.Menu();
+            bool stop = false;
+            Console.WriteLine(mySimulator.Report());
+            do
+            {
+                Console.WriteLine("M for move, L for Left, R for Right");
+                string command = Console.ReadLine();
+                mySimulator.Play(command);
+            } while (stop != true);
+            /*
+            // 5x5 table
+            Grid myTable = new Grid(5, 5);
+            // robot starting from 0, 0, North position
+            Robot myRobot = new Robot( 0, 0, Direction.North);
+           
+            bool valid = myTable.IsValidPosition(myRobot.X,myRobot.Y);
+            Console.WriteLine(valid);
+            Robot myRobot2 = new Robot(0, -1, Direction.North);
+            Console.WriteLine(myTable.IsValidPosition(myRobot2.X, myRobot2.Y));
+
+            myRobot.Move();
+            Console.WriteLine(myRobot.Report()); // North, 0, 1
+
+            myRobot.TurnLeft();
+            Console.WriteLine(myRobot.Report()); // West, 0, 1
+
+            myRobot.TurnRight();
+            Console.WriteLine(myRobot.Report()); // North, 0, 1
+
+            Simulator mySimulator = new Simulator();
+            mySimulator.Place(-1, 2, Direction.East);
+            */
+        }
+    }
+}
---
+++ ThirdIteration/Simulator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Simulator
+    {
+        Grid myTable = new Grid(5, 5);
+        // robot starting from 0, 0, North position
+        Robot myRobot;
+        Direction setDirection;
+        public Simulator()
+        {
+
+        }
+        public void Menu()
+        {
+            Console.WriteLine("Instructions");
+            Console.WriteLine("Place the Robot on Table");
+            Console.WriteLine("Enter X position");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y position");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type Direction");
+            Console.WriteLine("Type N for North\n" +
+                 "Type W for West\n" +
+                 "Type E for East\n" +
+                 "Type S for South\n");
+            string strDirection = Console.ReadLine();
+            Place(x, y, strDirection);
+        }
+        public void Place(int x, int y, string strDirection)
+        {
+            switch (strDirection)
+            {
+                case "N": setDirection= Direction.North; break;
+                case "E": setDirection = Direction.East; break;
+                case "S": setDirection = Direction.South; break;
+                case "W": setDirection = Direction.West; break;
+            }
+            if (myTable.IsValidPosition(x, y))
+            {
+                myRobot = new Robot(x, y, setDirection);
+            }
+            else
+            {
+                Console.WriteLine("Invalid X or Y value");
+            }
+        }
+        public void Play(string instruction)
+        {
+            switch (instruction)
+            {
+                case "M":
+                    myRobot.Move();
+                    Console.WriteLine(myRobot.Report());
+                    break;
+                case "R":
+                    myRobot.TurnRight();
+                    Console.WriteLine(myRobot.Report());
+                    break;
+                case "L":
+                    myRobot.TurnLeft();
+                    Console.WriteLine(myRobot.Report());
+                    break;
+            } 
+        }
+
+        public string Report()
+        {
+            return myRobot.Report();
+        }
+     }
+}
